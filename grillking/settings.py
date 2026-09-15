5beb19f1bd41b36830171236fff548a03eb4e6b2
Lusius Zhang <zqiquan082@gmail.com>
2026-09-15 05:19:05 -0400

    Fix database connection usage

--- grillking/settings.py
+++ grillking/settings.py
@@ -120,7 +120,7 @@ WSGI_APPLICATION = "grillking.wsgi.application"
 DATABASES = {
     "default": dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
+        conn_max_age=0,
     )
 }
 
